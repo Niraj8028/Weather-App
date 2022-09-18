5bfc220a4e7b7cdbd1a46b95a5999537156cb420
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-19 00:23:50 +0530

    city suggestions done

---
+++ src/Components/Api.js
@@ -0,0 +1,12 @@
+ export const API_OPTIONS = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '425566b377mshe0f0ee201295fe1p1086cfjsnd447e41fe515',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+ export const API_KEY='https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
+
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,9 +1,29 @@
 import React,{useState} from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate'
+import { API_KEY } from './Api'
+import { API_OPTIONS } from './Api'
 
 function Searchbar({onSearchChange}) {
 
     const [search, setSearch] = useState(null)
+
+    const loadOptions=(inputValue)=>{
+        return(
+            fetch(`${API_KEY}/cities?minPopulation=1000000&namePrefix=${inputValue}`, API_OPTIONS)
+	    .then(response => response.json())
+	    .then(response => {
+            return {
+                options:response.data.map((city)=>{
+                    return{
+                        value:`${city.latitude} ${city.longitude}`,
+                        label:`${city.name},${city.countryCode}`
+                    }
+                })
+            }
+        })
+	    .catch(err => console.error(err))
+        )
+    }
 
     const handleChange=(searchData)=>{
         setSearch(searchData);
@@ -16,6 +36,7 @@ function Searchbar({onSearchChange}) {
     debounceTimeout={600}
     value={search}
     onChange={handleChange}
+    loadOptions={loadOptions}
     />
 
     
